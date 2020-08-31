260bd013d7422a064778322e13a64217d87a9210
mayowDev <dinnod4@gmail.com>
2020-09-01 00:30:28 +0500

    custom auth middleware and jwt verify

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,7 +12,7 @@ module.exports = function(req,res,next){
         next()
     }catch(err){
         res.status(401).json({msg: 'Token is not valid'})
-        console.error(err)
+        console.error(err.message)
         
     }
     
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../../middleware/auth')
+const User = require('../../models/User')
+
 
 
 /**
@@ -8,6 +11,16 @@ const router = express.Router()
  * @access Public
  */
 
-router.get('/', (req, res) => res.send('auth route'))
+router.get('/', auth, async (req, res) => {
+    
+    try{
+        const user = await User.findById(req.user.id).select('-password')
+        res.json(user)
+    }catch(err){
+        console.error(err.message)
+        res.status(500).send('server error')
+    }
+
+})
 
 module.exports = router;
