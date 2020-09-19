5f40127da62d1ae5e9ba88c548aa934bfd42240a
mayowDev <dinnod4@gmail.com>
2020-09-20 03:15:56 +0500

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 const express = require("express")
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express();
 //connect databse
 connectDB();
 //init middleware
 app.use(express.json({ extended: false}));
-app.get('/', (req, res)=> res.send('API working'))
-// define api endpin - troutes
+
+// define api endpoints - routes
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/posts', require('./routes/api/posts'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/auth', require('./routes/api/auth'))
 
-
-
+// server static assets in production
+if(process.env.NODE_ENV === production){
+    // set static folder
+    app.use(express.static('client/build'))
+    // redirect users with other endpoints
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 // listen to the server
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log( `Server started on port ${PORT}`))
